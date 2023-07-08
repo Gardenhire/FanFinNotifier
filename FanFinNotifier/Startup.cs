305534632385a0fc6a208349e1fce9f2f8d78f62
Kevin <kgardenhire16@gmail.com>
2023-07-07 23:44:19 -0500

    testing moving firebase

---
+++ FanFinNotifier/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FirebaseAdmin;
+using Google.Cloud.Functions.Hosting;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+
+namespace FanFinNotifier
+{
+	public class Startup: FunctionsStartup
+	{
+        public override void Configure(WebHostBuilderContext context, IApplicationBuilder app)
+        {
+            FirebaseApp.Create();
+        }
+    }
+}
+
